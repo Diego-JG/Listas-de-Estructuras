30b29c97193ed86202040fbe239da97f8ad92853
Diego-JG <holasoydiegojg@gmail.com>
2019-09-13 13:34:44 -0500

    Memoria dinamica o lista dinamica

--- main.cpp
+++ main.cpp
@@ -7,7 +7,48 @@ struct nodos{
 };
 
 int main () {
+
+	 
 	nodos dat1, dat2, dat3, dat4, dat5;
+		
+	struct nodos *inicio;
+	struct nodos  *aux;
+	
+	//auxiliares
+	aux= new nodos();
+	aux->valor=4;
+	aux->ptr=NULL;
+	//cout<<aux->valor;
+	
+	inicio=aux;
+	aux= new nodos();
+	aux->valor=5;
+	aux->ptr=NULL;
+	//cout<<aux->valor;
+	
+	inicio->ptr=aux;
+	aux= new nodos();
+	aux->valor=6;
+	aux->ptr=NULL;
+	//cout<<aux->valor;
+	
+	inicio->ptr->ptr=aux;
+	aux= new nodos();
+	aux->valor=7;
+	aux->ptr=NULL;
+	//cout<<aux->valor;
+	
+	inicio->ptr->ptr->ptr=aux;
+	aux= new nodos();
+	aux->valor=8;
+	aux->ptr=NULL;
+	
+	inicio->ptr->ptr->ptr->ptr=aux;
+	//cout<<aux->valor;
+	
+
+	
+	
 	dat1.valor=3;
 	dat1.ptr=NULL;
 	dat1.ptr=&dat2;
@@ -27,12 +68,28 @@ int main () {
 	dat5.ptr=NULL;
 
 	
-	
+	cout<<"apuntador que apunta apuntador";
 	cout<<dat1.valor<<endl;
 	cout<<dat1.ptr->valor<<endl;
 	cout<<dat1.ptr->ptr->valor<<endl;
 	cout<<dat1.ptr->ptr->ptr->valor<<endl;
 	cout<<dat1.ptr->ptr->ptr->ptr->valor<<endl;
+	
+	cout<<"lista dinamica"<<endl;
+	cout<<inicio->valor<<endl;
+	cout<<inicio->ptr->valor<<endl;
+	cout<<inicio->ptr->ptr->valor<<endl;
+	cout<<inicio->ptr->ptr->ptr->valor<<endl;
+	cout<<inicio->ptr->ptr->ptr->ptr->valor<<endl;
+	
+	
+	
+	
+
+	
+
+	
+
 
 	
 	return 0;
